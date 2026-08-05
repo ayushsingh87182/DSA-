62223feccb9a70319b27a404315c0df1605bd820
ayushsingh87182 <ayush.ranchi.2506@gmail.com>
2026-08-05 18:42:25 +0530

    5/8/26 - 1903

---
+++ StringgOdd.java
@@ -0,0 +1,24 @@
+
+public class StringgOdd {
+
+    public String largestOddNumber(String num) {
+        int i = num.length() - 1;
+
+        while (i >= 0 && (num.charAt(i) - '0') % 2 == 0) {
+            i--;
+        }
+
+        return num.substring(0, i + 1);
+    }
+
+    public static void main(String[] args) {
+        StringgOdd o = new StringgOdd();
+
+        String num = "3540";
+
+        String result = o.largestOddNumber(num);
+
+        System.out.println("Input: " + num);
+        System.out.println("Largest Odd Number: " + result);
+    }
+}
