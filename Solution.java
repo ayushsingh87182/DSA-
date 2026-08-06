fb58703d44ac6a6169309d64fbdcbd4d40720123
ayushsingh87182 <ayush.ranchi.2506@gmail.com>
2026-08-06 18:27:20 +0530

    06/08/26-125

---
+++ Solution.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        Solution obj = new Solution();
+
+        System.out.println(obj.isPalindrome(s));
+
+        sc.close();
+    }
+}
